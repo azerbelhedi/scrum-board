fef2fca4e3955adf3a263d63a11fd8c79517323b
azerbelhedi <azerbelhedide@gmail.com>
2019-09-21 22:00:02 +0100

    navbar ui

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,62 @@
-import React , { Wrapper } from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Form, Button } from "react-bootstrap";
+import "./NavBar.css";
 
 const NavBar = () => {
-    return(
-       <div>
-            <Link to = "/"> Home </Link>
-            <Link to = "/backlog"> Backlog </Link>
-            <Link to = "/user-story"> User Story </Link>
-            <Link to = "/sprint" > Sprint </Link>
-       </div>
-    )
-}
+  return (
+    <div>
+      <Navbar className="nav-bar" expand="md">
+        <Navbar.Brand>
+          <Link to="/" className="title">
+            {" "}
+            SB
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">
+              <Link to="/" className="nav-item">
+                {" "}
+                Board{" "}
+              </Link>
+            </Nav.Link>
+            <Nav.Link href="#link">
+              <Link to="/backlog" className="nav-item">
+                {" "}
+                Backlog{" "}
+              </Link>
+            </Nav.Link>
+            <Nav.Link href="#link">
+              <Link to="/user-story" className="nav-item">
+                {" "}
+                User Story{" "}
+              </Link>
+            </Nav.Link>
 
-export default NavBar ;
+            <Nav.Link href="#link">
+              <Link to="/sprint" className="nav-item">
+                {" "}
+                Sprint{" "}
+              </Link>
+            </Nav.Link>
+          </Nav>
+          <Form inline>
+            <Nav.Link href="#link">
+              <Link to="/login" className="nav-item">
+                {" "}
+                <Button variant="outline-info" className="nav-button">
+                  Sign In
+                </Button>
+              </Link>
+            </Nav.Link>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router,  Route } from 'react-router-dom'
-import Home from '../Home/Home'
-import Login from '../Login/Login'
-import Backlog from '../Backlog/Backlog'
-import Sprint from '../Sprint/Sprint'
-import NavBar from '../NavBar/NavBar'
-import UserStory from '../UserStory/UserStory'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Login from "../Login/Login";
+import Backlog from "../Backlog/Backlog";
+import Sprint from "../Sprint/Sprint";
+import NavBar from "../NavBar/NavBar";
+import UserStory from "../UserStory/UserStory";
 
-const Routes = (props) =>{
-    return(
-        <h3>
-            <Router>
-                <NavBar/>
-                <Route exact path = "/" component = {Home}/>
-                <Route path = "/login" component = {Login} />
-                <Route path = "/backlog" component = {Backlog} /> 
-                <Route path = "/sprint" component = {Sprint} />
-                <Route path = "/user-story" component = {UserStory} />
-            </Router>
-        </h3>
-    )
-}
+const Routes = props => {
+  return (
+    <h3>
+      <Router>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/backlog" component={Backlog} />
+        <Route path="/sprint" component={Sprint} />{" "}
+        <Route path="/user-story" component={UserStory} />
+      </Router>
+    </h3>
+  );
+};
 
-export default Routes ;
+export default Routes;
